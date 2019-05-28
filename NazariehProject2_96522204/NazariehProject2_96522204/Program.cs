49675ab06e822cd4caf357d9e296f147c95a93b3
Mina Tahaei <bluxixi@outlook.com>
2019-05-28 08:59:34 +0430

    Read input

--- NazariehProject2_96522204/NazariehProject2_96522204/Program.cs
+++ NazariehProject2_96522204/NazariehProject2_96522204/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace NazariehProject2_96522204
 {
@@ -10,6 +11,26 @@ namespace NazariehProject2_96522204
     {
         static void Main(string[] args)
         {
+            var ReadData = ReadFile();
+        }
+        public static string[] ReadFile()
+        {
+            List<string> SaveList = new List<string>();
+
+            using (StreamReader sr = new StreamReader(@"..\..\input.txt"))
+            {
+
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+
+                    SaveList.Add(line);
+
+                }
+            }
+            return SaveList.ToArray();
+
         }
     }
 }
